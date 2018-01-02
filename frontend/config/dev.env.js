eb8dbbdfbe9afacc24a5802c72a8394fef89442b
wmhello <871228582@qq.com>
2018-01-02 22:22:48 +0800

    前端新功能发布 角色功能完善

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://web.ynxpyz.cn"'
+  BASE_API: '"http://wmhello.natapp1.cc"'
 })
